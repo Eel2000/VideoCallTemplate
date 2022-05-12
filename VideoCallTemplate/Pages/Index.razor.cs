3c7571d972c2b5e653e40fe835c0549d04df867d
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-05-12 16:12:57 +0100

    Add project files.

---
+++ VideoCallTemplate/Pages/Index.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace VideoCallTemplate.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime? jSRuntime { get; set; }
+
+        private bool IsInCall = false;
+
+        string icon = MudBlazor.Icons.Filled.Call;
+        MudBlazor.Color Color = MudBlazor.Color.Success;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+        }
+
+        private async void Call()
+        {
+            IsInCall = !IsInCall;
+            icon = IsInCall ? MudBlazor.Icons.Filled.CallEnd : MudBlazor.Icons.Filled.Call;
+            Color = IsInCall ? MudBlazor.Color.Error : MudBlazor.Color.Success;
+            if (IsInCall)
+            {
+                await jSRuntime!.InvokeVoidAsync("openCam");
+                //var data = await jSRuntime.InvokeAsync<string>("processStream");
+            }
+        }
+    }
+}
---
+++ VideoCallTemplate/wwwroot/Js/camera.js
@@ -0,0 +1,62 @@
+﻿let video = null;
+let documentImage = null;
+
+function openCam() {
+    video = document.getElementById('video');
+    documentImage = document.getElementById('cam');
+    if (video != null) {
+        console.log("Ok")
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            console.log("media avaible")
+            navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+                .then(function (stream) {
+                    console.log("stream available will proceed to cam process")
+                    video.srcObject = stream;
+                    video.play()
+                })
+                .catch(function (error) {
+                    console.log("An Error occured while processing...")
+                })
+        }
+    }
+    else {
+        console.error("no ok");
+    }
+    video.onplay = function () {
+        //var vStream = video.captureStream();
+        /*  console.log("stream captured =>" + JSON.stringify(vStream));*/
+        processStream();
+    }
+}
+
+
+
+
+function processStream() {
+    var innerCanvas = document.createElement('canvas');
+    innerCanvas.width = 400;
+    innerCanvas.height = 400;
+    var innerContext = innerCanvas.getContext('2d');
+    var image = new Image();
+    setInterval(() => {
+        const ratio = image.width / image.height;
+        let newWidth = innerCanvas.width;
+        let newHeight = newWidth / ratio;
+
+        if (newHeight < innerCanvas.height) {
+            newHeight = innerCanvas.height;
+            newWidth = newHeight * ratio;
+        }
+
+        const xOffest = newWidth > innerCanvas.width ? (innerCanvas.width - newWidth) / 2 : 0;
+        const yOffest = newHeight > innerCanvas.height ? (innerCanvas.height - newHeight) / 2 : 0;
+
+        innerContext.drawImage(video, xOffest, yOffest, newWidth, newHeight);
+        image.src = innerCanvas.toDataURL('image/png', 0.2);
+
+        documentImage.src = image.src;
+        /*console.log(JSON.stringify(image.src));*/
+    }, 50);
+
+    return innerCanvas.toDataURL('image/png', 0.2);
+}
